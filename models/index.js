71314e3258aacccf998aa9d5086982235896cf4a
Brenton Turnor <brenton.turnor@hotmail.com>
2021-08-29 12:58:15 +0800

    updating index.js model and server.js

--- models/index.js
+++ models/index.js
@@ -8,19 +8,23 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: "ProductTag",
+  through: ProductTag,
+  foreignKey: "product_id"
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: "ProductTag",
+  through: ProductTag,
+  foreignKey: "tag_id"
 });
 
 module.exports = {
